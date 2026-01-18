13b738f43175028bc5c85b52749d1565d5e549e3
Koloina01 <kolomaherison@gmail.com>
2026-01-18 17:21:58 +0300

    feat: add table dishIngredient

---
+++ src/main/resources/sql/new_schema.sql
@@ -0,0 +1,11 @@
+alter table ingredient drop column if exists id_dish;
+alter table ingredient drop column if exists required_quantity;
+create type unit_type as enum ('PCS', 'KG', 'L');
+
+create table DishIngredient (
+    id serial primary key,
+    id_dish int references dish (id),
+    id_ingredient int references ingredient (id),
+    quantity_required numeric(10, 2),
+    unit unit_type
+);
