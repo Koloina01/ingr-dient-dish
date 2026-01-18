063aea3b47322273c369e3425bf7c0d9c293914b
Koloina01 <kolomaherison@gmail.com>
2026-01-18 17:38:47 +0300

    fix: add column selling_price

--- src/main/resources/sql/new_schema.sql
+++ src/main/resources/sql/new_schema.sql
@@ -1,6 +1,8 @@
 alter table ingredient drop column if exists id_dish;
 alter table ingredient drop column if exists required_quantity;
 create type unit_type as enum ('PCS', 'KG', 'L');
+alter table dish add column selling_price numeric(10,2);
+
 
 create table DishIngredient (
     id serial primary key,
